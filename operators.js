bacf6d60a83006b88e3c50a4b32aa583eeef2983
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-07-06 17:21:06 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,26 @@
+// Operators in JS
+
+console.log(`Operators in JS`);
+
+// Arithmetic Operators
+
+let x = 10,
+  y = 3;
+
+console.log(`${x} + ${y} = ${x + y}`);
+console.log(`${x} - ${y} = ${x - y}`);
+console.log(`${x} * ${y} = ${x * y}`);
+console.log(`${x} / ${y} = ${x / y}`);
+console.log(`${x} % ${y} = ${x % y}`);
+console.log(`${x} ** ${y} = ${x ** y}`);
+
+console.log(`Increment in ${x++} = ${x}`);
+console.log(`Increment in ${x} = ${++x}`);
+
+console.log(`Decrement in ${y--} = ${y}`);
+console.log(`Decrement in ${y} = ${--y}`);
+
+// Assignment Operators
+
+let a = 5,
+  b = 2;
