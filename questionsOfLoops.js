599d9ebd0fc85aec284f1c6c617e1bfacb4e7497
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-07-07 18:10:43 +0530

    Practice questions of Loops

--- questionsOfLoops.js
+++ questionsOfLoops.js
@@ -49,11 +49,7 @@ for (let i = 1; i < n; i++) {
 }
 console.log(`Factorial of ${n} is ${factorial}`);
 
-// 5. Reverse a String: Write a loop that takes a string as input and prints it in reverse order.
-
 // 6. Count Characters: Write a loop that counts the number of occurrences of a specific character in a given string.
-
-// 7. Fibonacci Sequence: Write a loop that generates and prints the first 10 numbers in the Fibonacci sequence. The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones.
 
 // 8. Find Prime Numbers: Write a loop that finds and prints all prime numbers between 1 and 50.
 
@@ -86,8 +82,6 @@ console.log(numbers);
 
 // 14. Character Frequency: Write a loop that counts the frequency of each character in a given string and prints the result. ([for-of loop])
 
-// 15. Array Reversal: Write a loop that reverses the order of elements in an array. ([for loop or while loop])
-
 // 16. Number Pattern: Write a loop that prints the numbers in the pattern 1, 2, 3, 4, 3, 2, 1. ([for loop])
 
 console.log(
@@ -118,8 +112,6 @@ while (x <= 10) {
 }
 console.log(factN);
 
-// 18. String Uppercase: Write a loop that converts all characters in a string to uppercase. ([for loop or for-of loop])
-
 // 19. Table of Squares: Write a loop that prints a table of squares from 1 to 10. ([for loop])
 
 console.log(`Square`);
@@ -130,4 +122,74 @@ for (let i = 1; i <= squareNum; i++) {
   console.log(`The square of ${i} is ${i * i}`);
 }
 
+// 18. String Uppercase: Write a loop that converts all characters in a string to uppercase. ([for loop or for-of loop])
+
+console.log(`Converting all the characters of a string to uppercase`);
+
+let str = "tanmay";
+
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i].toUpperCase());
+}
+
+// 5. Reverse a String: Write a loop that takes a string as input and prints it in reverse order.
+
+console.log(`Reversing a string`);
+
+str = "Tanmay";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  console.log(str[i]);
+  // console.log(i);
+}
+
+// 15. Array Reversal: Write a loop that reverses the order of elements in an array. ([for loop or while loop])
+
+console.log(`Reversing the order of elements of an array`);
+
+let loopArr = ["JavaScript", "Python", "Java", "C++"];
+console.log(loopArr);
+console.log(loopArr.length);
+
+for (let i = loopArr.length - 1; i >= 0; i--) {
+  console.log(loopArr[i]);
+}
+
+// 7. Fibonacci Sequence: Write a loop that generates and prints the first 10 numbers in the Fibonacci sequence. The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones.
+
+console.log(`Fibonacci Sequence`);
+
+let fibonacci = 15;
+let num1 = 0;
+let num2 = 1;
+
+for (let i = 1; i <= fibonacci; i++) {
+  const num3 = num1 + num2;
+  num1 = num2;
+  num2 = num3;
+  console.log(num3);
+}
+
 // 20. Vowel Count: Write a loop that counts the number of vowels (a, e, i, o, u) in a given string. ([for-of loop])
+
+console.log(`Vowel Counter`);
+
+let vowelStr = "Tanmay Upreti";
+let vowelCount = 0;
+
+for (let character of vowelStr) {
+  let char = character.toLowerCase();
+  console.log(char);
+
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    vowelCount++;
+  }
+}
+
+console.log(`The total number of vowels in ${vowelStr} are ${vowelCount} `);
