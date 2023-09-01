b7fef939c430e7a0da2165234655119e8f005b9f
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-09-01 16:55:33 +0530

    Objects

---
+++ js/objects.js
@@ -0,0 +1,12 @@
+console.log(`Objects in JS`);
+
+let obj = {
+  name: "Tanmay",
+  class: "X A",
+};
+console.log(obj);
+console.log(typeof obj);
+console.log(obj.class);
+console.log(obj.name);
+console.log(obj["class"]);
+console.log(obj["name"]);
