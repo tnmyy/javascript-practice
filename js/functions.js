9f85d2f403e7bc21cb49bfbf14f12257b788b22e
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-09-07 15:31:23 +0530

    JS Functions

--- js/functions.js
+++ js/functions.js
@@ -4,14 +4,3 @@ function myFx() {
   console.log(`A JS Function`);
 }
 myFx();
-
-// return stops a function
-
-function sum(a, b, c) {
-  let sum = a + b + c;
-  sum = Number.parseFloat(sum).toFixed(3);
-  console.log(`The sum of three numbers ${a}, ${b} and ${c} is: ${sum}`);
-  return sum;
-}
-
-console.log(sum(2, 8.66e4 * 1.0256, 3));
