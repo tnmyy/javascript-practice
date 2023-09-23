7eb02f5b7b82c7f91b68648d81935102c8652f8d
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-09-23 15:58:56 +0530

    JS Promises

--- js/async.js
+++ js/async.js
@@ -55,3 +55,122 @@ myFnc = function () {
 };
 // setInterval(myFnc, 1000);
 myFnc();
+
+// 3. JS Promises
+
+console.log(`3. JS Promises`);
+
+let myProm = new Promise((resolve, reject) => {
+  // Producing Code
+
+  resolve(); // When Successful
+  reject(); // When error
+});
+
+// Consuming Code
+
+myProm.then(
+  (val) => {
+    console.log(`Success`);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+function display(some) {
+  console.log(some);
+}
+
+myProm = new Promise((resolve, reject) => {
+  let x = 0;
+
+  if (x === 0) {
+    resolve(`Correct Value`);
+  } else {
+    reject(`Change the value`);
+  }
+});
+
+myProm.then(
+  function (val) {
+    display(val);
+  },
+  function (err) {
+    display(err);
+  }
+);
+
+// Waiting for a TimeOut
+
+// By Callback
+setTimeout(() => {
+  myFunction("I love you!!!");
+}, 3000);
+
+function myFunction(val) {
+  console.log(val);
+}
+
+// By Promises
+
+myProm = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("I love you!!!");
+  }, 3000);
+});
+
+myProm.then((val) => {
+  console.log(val);
+});
+
+// Waiting For a File
+
+// By callback
+
+function getFile(cb) {
+  let req = new XMLHttpRequest();
+
+  req.open("GET", "../js/async.js");
+  req.onload = function () {
+    if (req.status == 200) {
+      cb(req.responseText);
+    } else {
+      cb("Error: " + req.status);
+    }
+  };
+
+  req.send();
+}
+
+function fileDisplay(file) {
+  document.getElementById("body").style.background = "#fff";
+  document.getElementById("body").innerHTML = file;
+}
+
+// getFile(fileDisplay);
+
+// By Promise
+myProm = new Promise((resolve, reject) => {
+  let req = new XMLHttpRequest();
+
+  req.open("GET", "../js/async.js");
+  req.onload = function () {
+    if (req.status == 200) {
+      resolve(req.responseText);
+    } else {
+      reject("Error: " + req.status);
+    }
+  };
+
+  req.send();
+});
+
+myProm.then(
+  (val) => {
+    fileDisplay(val);
+  },
+  (err) => {
+    fileDisplay(err);
+  }
+);
